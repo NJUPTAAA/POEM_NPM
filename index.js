8f8d31531cbab230fcdd3191ba3ff4ae2d3d2139
ZsgsDesign <zsgsdesign@gmail.com>
2019-07-01 17:48:53 +0800

    fixed bugs

--- index.js
+++ index.js
@@ -12,17 +12,30 @@ exports.parse = function (_poemPath, type="auto") {
     if (type=="poem") {
         ret=JSON.parse(fs.readFileSync(_poemPath));
     } else if (type=="poetry") {
+        ret=[];
         let tmpPath=__dirname + "/tmp/" + new Date().getTime() + "/";
         if (!fs.existsSync(tmpPath)) {
             fs.mkdirSync(tmpPath);
         }
-        fs.createReadStream(_poemPath).pipe(unzip.Parse()).pipe(fstream.Writer(tmpPath));
-        ret=[];
+        var procced=false;
+        fs.createReadStream(_poemPath).pipe(unzip.Parse()).on('entry', function (entry) {
+            if(procced==true) entry.autodrain();
+            else {
+                let fileName = entry.path;
+                let type = entry.type;
+                let size = entry.size;
+                if(type=='File'){
+                    entry.pipe(fs.createWriteStream(tmpPath+fileName));
+                    procced=true;
+                    ret=JSON.parse(fs.readFileSync(tmpPath+fileName));
+                }
+            }
+        });
     }
     console.log(ret);
     return ret;
 };
 
 exports.generate = function (_poemRaw, type="poem") {
-    console.log("This is my first module");
+    console.log("Not Finished");
 };
